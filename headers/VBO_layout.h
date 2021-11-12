1ae8b42bb2bda96f9b7c79d93778dcddbb304440
Seb James <seb.james@sheffield.ac.uk>
2021-11-12 14:18:35 +0000

    Another few tabs removed

--- headers/VBO_layout.h
+++ headers/VBO_layout.h
@@ -10,7 +10,7 @@ struct VBO_element {
 
     static unsigned int getSizeOfType(unsigned int type) {
         switch (type) {
-        case GL_FLOAT:			return 4;
+        case GL_FLOAT:          return 4;
         case GL_UNSIGNED_INT:   return 4;
         case GL_UNSIGNED_BYTE:  return 1;
         }
